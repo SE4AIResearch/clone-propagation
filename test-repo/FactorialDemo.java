4323e82b6a2d8266ec11e1b35cd6c992896a03f2
vaansh3 <vaansh3@gmail.com>
2026-07-31 21:04:13 -0400

    Add FactorialDemo class with loop and recursive methods

---
+++ test-repo/FactorialDemo.java
@@ -0,0 +1,17 @@
+public class FactorialDemo {
+
+    public int factorialLoop(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public int factorialRecursive(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * factorialRecursive(n - 1);
+    }
+}
