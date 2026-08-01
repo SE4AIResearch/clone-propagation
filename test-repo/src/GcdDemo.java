b89bd29e6c8f3fefd2e4671f7ddf042fd348171b
vaansh3 <vaansh3@gmail.com>
2026-07-31 21:07:00 -0400

    Add GcdDemo class with iterative and recursive GCD methods

---
+++ test-repo/src/GcdDemo.java
@@ -0,0 +1,16 @@
+public class GcdDemo {
+
+    public int gcdIterative(int a, int b) {
+        while (b != 0) {
+            int t = b;
+            b = a % b;
+            a = t;
+        }
+        return a;
+    }
+
+    public int gcdRecursive(int a, int b) {
+        if (b == 0) return a;
+        return gcdRecursive(b, a % b);
+    }
+}
