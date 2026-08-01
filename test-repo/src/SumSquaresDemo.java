9aaa787dfbd3cd446786e6b1b6ea8a8687374f30
vaansh3 <vaansh3@gmail.com>
2026-07-31 21:11:54 -0400

    Add SumSquaresDemo class with sum of squares methods

---
+++ test-repo/src/SumSquaresDemo.java
@@ -0,0 +1,18 @@
+public class SumSquaresDemo {
+
+    public int sumOfSquares(int n) {
+        int total = 0;
+        for (int i = 1; i <= n; i++) {
+            total += i * i;
+        }
+        return total;
+    }
+
+    public int sumOfSquaresExact(int n) {
+        int total = 0;
+        for (int i = 1; i <= n; i++) {
+            total += i * i;
+        }
+        return total;
+    }
+}
