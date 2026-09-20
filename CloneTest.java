e053a162e887dc28770ab5de8bccae46e68bd0f1
vaansh3 <vaansh3@gmail.com>
2026-09-20 15:57:19 -0400

    Implement findMax and findMaxSafe methods
    Added a safe method to find the maximum value in an array.

---
+++ CloneTest.java
@@ -0,0 +1,25 @@
+public class CloneTest {
+
+    public int findMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public int findMaxSafe(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
